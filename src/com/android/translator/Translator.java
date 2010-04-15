302160ee4ed6b155e60870fe009c7548b34ee8c5
Damien Mathieu <damien.mathieu@lim.eu>
2010-04-15 17:57:12 +0200

    display languages spinners

--- src/com/android/translator/Translator.java
+++ src/com/android/translator/Translator.java
@@ -3,13 +3,24 @@ package com.android.translator;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Translator extends Activity
-{
-    /** Called when the activity is first created. */
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
+
+public class Translator extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.languages_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        
+        Spinner languages_from = (Spinner) findViewById(R.id.languages_from);
+        languages_from.setAdapter(adapter);
+        
+        Spinner languages_to = (Spinner) findViewById(R.id.languages_to);
+        languages_to.setAdapter(adapter);
     }
 }

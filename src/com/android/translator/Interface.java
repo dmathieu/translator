ad9b3762e5e33602cdea2fae7ec7702656f74c55
Damien Mathieu <damien.mathieu@lim.eu>
2010-04-26 13:32:31 +0200

    cleaning up and comments

---
+++ src/com/android/translator/Interface.java
@@ -0,0 +1,39 @@
+package com.android.translator;
+
+import android.app.Activity;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.api.translate.Language;
+
+
+public class Interface {
+	
+	/*
+	 * Instantiates the spinner with it's content
+	 */
+    public static ArrayAdapter getSpinnerAdapter(Activity context, Boolean has_guess) {
+    	ArrayAdapter adapter = new ArrayAdapter(context, android.R.layout.simple_spinner_item);
+    	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    	fillSpinner(adapter, has_guess);
+        return adapter;
+    }
+    
+    /*
+	 * Fills the spinner with the languages list
+	 */
+	public static void fillSpinner(ArrayAdapter adapter, Boolean has_guess) {
+		adapter.clear();
+		
+		Integer i = 0;
+		for (Language l : Language.values()) {
+			if (i > 0 || has_guess == true) {
+				adapter.add(l.name());
+			}			
+			i += 1;
+		}
+	}
+}
--- src/com/android/translator/Main.java
+++ src/com/android/translator/Main.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.api.translate.Language;
-import com.google.api.translate.Translate;
 
 
 public class Main extends Activity {
@@ -20,10 +19,17 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        
-        ((Spinner) findViewById(R.id.languages_from)).setAdapter(Translator.getSpinnerAdapter(this, true));
-        ((Spinner) findViewById(R.id.languages_to)).setAdapter(Translator.getSpinnerAdapter(this, false));
+        /*
+         * We get the two spinners : the language to translate from
+         * And the language to translate to. And we add them the languages list
+         */
+        ((Spinner) findViewById(R.id.languages_from)).setAdapter(Interface.getSpinnerAdapter(this, true));
+        ((Spinner) findViewById(R.id.languages_to)).setAdapter(Interface.getSpinnerAdapter(this, false));
 		
+        
+        /*
+         * Whenever we click on the button, we translate the string.
+         */
 		findViewById(R.id.translate_button).setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				String text = ((EditText) findViewById(R.id.text_to_translate)).getText().toString();
@@ -35,49 +41,12 @@ public class Main extends Activity {
 					String translatedText = Translator.translate(text, lang_from, lang_to);
 					((TextView) findViewById(R.id.translated_text)).setText(translatedText);
 				} catch(Exception e) {
+					/*
+					 * It failed. We display the error message
+					 */
 					((TextView) findViewById(R.id.translated_text)).setText(e.toString());
 				}
 			}
 		});
     }
-    
-    public static ArrayAdapter getSpinnerAdapter(Activity context, Boolean has_guess) {
-    	ArrayAdapter adapter = new ArrayAdapter(context, android.R.layout.simple_spinner_item);
-    	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    	Translator.fillSpinner(adapter, has_guess);
-        return adapter;
-    }
-	
-	public static String translate(String text, Language lang_from, Language lang_to) {
-		try {
-			Translate.setHttpReferrer("http//www.dmathieu.com");
-			String translatedText = Translate.execute(text, lang_from, lang_to);
-			return translatedText;
-		} catch(Exception e) {
-			return "An error occured. Can't translate (from " + lang_from + " to " + lang_to + "): " + e.toString();
-		}
-	}
-	
-	public static void fillSpinner(ArrayAdapter adapter, Boolean has_guess) {
-		adapter.clear();
-		
-		Integer i = 0;
-		for (Language l : Language.values()) {
-			if (i > 0 || has_guess == true) {
-				adapter.add(l.name());
-			}			
-			i += 1;
-		}
-	}
-	
-	public static Language getLanguageString(Spinner spinner) throws Exception {
-		String lang = spinner.getSelectedItem().toString();
-		
-		for (Language l : Language.values()) {
-			if (l.name() == lang) {
-				return l;
-			}
-		}
-		throw new Exception("Unknown language provided : " + lang);
-	}
 }

d21c0c03ed128a9eb7d448594a87b94b82d86e11
dmathieu <42@dmathieu.com>
2010-05-06 18:49:39 +0200

    this time it's a real rename

---
+++ src/com/dmathieu/translator/Main.java
@@ -0,0 +1,52 @@
+package com.translator.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.api.translate.Language;
+
+
+public class Main extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        /*
+         * We get the two spinners : the language to translate from
+         * And the language to translate to. And we add them the languages list
+         */
+        ((Spinner) findViewById(R.id.languages_from)).setAdapter(Interface.getSpinnerAdapter(this, true));
+        ((Spinner) findViewById(R.id.languages_to)).setAdapter(Interface.getSpinnerAdapter(this, false));
+		
+        
+        /*
+         * Whenever we click on the button, we translate the string.
+         */
+		findViewById(R.id.translate_button).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String text = ((EditText) findViewById(R.id.text_to_translate)).getText().toString();
+				
+				try {
+					Language lang_from = Translator.getLanguageString((Spinner) findViewById(R.id.languages_from));
+					Language lang_to = Translator.getLanguageString((Spinner) findViewById(R.id.languages_to));
+					
+					String translatedText = Translator.translate(text, lang_from, lang_to);
+					((TextView) findViewById(R.id.translated_text)).setText(translatedText);
+				} catch(Exception e) {
+					/*
+					 * It failed. We display the error message
+					 */
+					((TextView) findViewById(R.id.translated_text)).setText(e.toString());
+				}
+			}
+		});
+    }
+}
---
+++ src/com/dmathieu/translator/Translator.java
@@ -0,0 +1,32 @@
+package com.translator.android;
+
+import android.widget.Spinner;
+
+import com.google.api.translate.Language;
+import com.google.api.translate.Translate;
+
+
+public class Translator {
+	/*
+	 * Takes the string to translate and does the work
+	 */
+	public static String translate(String text, Language lang_from, Language lang_to) throws Exception {
+		Translate.setHttpReferrer("http//www.dmathieu.com");
+		String translatedText = Translate.execute(text, lang_from, lang_to);
+		return translatedText;
+	}
+	
+	/*
+	 * Gets a language string from it's name (ENGLISH -> en)
+	 */
+	public static Language getLanguageString(Spinner spinner) throws Exception {
+		String lang = spinner.getSelectedItem().toString();
+		
+		for (Language l : Language.values()) {
+			if (l.name() == lang) {
+				return l;
+			}
+		}
+		throw new Exception("Unknown language provided : " + lang);
+	}
+}
